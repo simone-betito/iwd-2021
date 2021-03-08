efca4d580d862bca1da1c464fdaeb9fd7ab0e293
Simone <betis855@newschool.com>
2021-03-08 16:25:52 -0500

    changes

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 const container = document.querySelector(".waffle");
-const colorGen = d3.interpolateLab("#364958", "#86bbbd");
+const colorGen = d3.interpolateLab("#6679CC", "#c6d2ed");
 
-[...Array(130)].forEach((block, i, arr) => {
+[...Array(346)].forEach((block, i, arr) => {
   const progress = (i + 1) / arr.length;
   const color = colorGen(progress);
-  container.innerHTML += `<div class="${
-    i > 99 ? "block drop" : "block"
-  }" style="background-color: ${color}"></div>`;
+  container.innerHTML += `<div class="block" style="background-color: ${color}"></div>`;
 });
 
 const headline = document.querySelector(".state");
